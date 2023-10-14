7ac2a23b8a4812972e70ee0634b3a942febee6bc
Andrei Slapik <Slapon4ik92@gmail.com>
2023-10-14 11:37:34 +0300

    refactor test 5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,5 +1,5 @@
 
-let rule = "Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом.";
+let rule = "Еще не родился тот человек, который, поста";
 
 switch (true) {
   case rule.length < 25:
@@ -8,7 +8,7 @@ switch (true) {
   case rule.length > 25:
     console.log('У меня 100 пудов все получится')
     break
-  default:
-    console.log('50 на 50')
-    break
+    case rule.length === 25:
+      console.log('50 на 50')
+      break
 }
